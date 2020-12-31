09bda6b27f5f14bb41d899fac2b90da1d2920e31
Luke <30535366+Luky584@users.noreply.github.com>
2020-12-31 16:19:51 +0100

    Revritten to use state

--- src/components/BarContainer.js
+++ src/components/BarContainer.js
@@ -3,6 +3,38 @@ import React, { Component } from 'react';
 
 class BarContainer extends Component {
  
+    constructor(props) {
+    super(props);
+    this.state = {
+        examParts: [
+            {
+                title: "Introduction",
+                duration: 0.5,
+            },
+            {
+                title: "P1 Questions",
+                duration: 3,
+            },
+            {
+                title: "P2 Task1",
+                duration: 2,
+            },
+            {
+                title: "P2 Task2",
+                duration: 2,
+            },
+            {
+                title: "P3 TOPIC",
+                duration: 5,
+            },
+            {
+                title: "P4 TEXT",
+                duration: 3,
+            },
+        ]
+    };
+    }
+
   componentDidMount() {
       console.log("barcontainer mounted");
   }
@@ -12,41 +44,12 @@ class BarContainer extends Component {
   }
 
   render() {
-    const examParts = [
-        {
-            title: "Introduction",
-            duration: 0.5,
-        },
-        {
-            title: "P1 Questions",
-            duration: 3,
-        },
-        {
-            title: "P2 Task1",
-            duration: 2,
-        },
-        {
-            title: "P2 Task2",
-            duration: 2,
-        },
-        {
-            title: "P3 TOPIC",
-            duration: 5,
-        },
-        {
-            title: "P4 TEXT",
-            duration: 3,
-        },
-    ];
-
     let totalDuration = 0;
-    examParts.map((exam) =>
+    this.state.examParts.map((exam) =>
         totalDuration+=exam.duration
     );
-    
-    console.log({totalDuration});
 
-    let barParts = examParts.map((exam) =>
+    let barParts = this.state.examParts.map((exam) =>
         <div className="bar" style={{width:(exam.duration/totalDuration)*100 + "%"}}>{exam.title}</div> 
     );
 

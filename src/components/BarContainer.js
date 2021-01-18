adee89d99238b9194bcf728c9791670c5d3d827c
Luke <30535366+Luky584@users.noreply.github.com>
2021-01-18 10:23:11 +0100

    Basic timer with controls

--- src/components/BarContainer.js
+++ src/components/BarContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Bar from "./Bar";
+import ProgressBar from "./ProgressBar";
 
 class BarContainer extends Component {
  
@@ -31,9 +32,37 @@ class BarContainer extends Component {
                 title: "P4 TEXT",
                 duration: 3,
             },
-        ]
+        ],
+        timerOn: false,
+        timerStart: 0,
+        timerTime: 0,
     };
-  }
+  };
+
+  startTimer = () => {
+    this.setState({
+      timerOn: true,
+      timerTime: this.state.timerTime,
+      timerStart: Date.now() - this.state.timerTime
+    });
+    this.timer = setInterval(() => {
+      this.setState({
+        timerTime: Date.now() - this.state.timerStart
+      });
+    }, 10);
+  };
+
+  stopTimer = () => {
+    this.setState({ timerOn: false });
+    clearInterval(this.timer);
+  };
+
+  resetTimer = () => {
+    this.setState({
+      timerStart: 0,
+      timerTime: 0
+    });
+  };
 
   componentDidMount() {
       console.log("barcontainer mounted");
@@ -44,14 +73,48 @@ class BarContainer extends Component {
   }
 
   render() {
+    const { timerTime } = this.state;
+    let totalExamTime = 0;
+    this.state.examParts.map((exam) => totalExamTime+= exam.duration); // calculate total exam duration
+
+    let centiseconds = ("0" + (Math.floor(timerTime / 10) % 100)).slice(-2);
+    let seconds = ("0" + (Math.floor(timerTime / 1000) % 60)).slice(-2);
+    let minutes = ("0" + (Math.floor(timerTime / 60000) % 60)).slice(-2);
+
+    let percentPassed = (timerTime/600)/totalExamTime;
+
+    let minutesLeft = Math.floor((totalExamTime*60*1000-timerTime)/60/1000);
+    let secondsLeft = ("0" + (Math.floor((totalExamTime*60*1000-timerTime) / 1000) % 60)).slice(-2);
+    let centiSecondsLeft = ("0" + (Math.floor((totalExamTime*60*1000-timerTime) / 10) % 100)).slice(-2);
     return (
+        <>
         <div className="bar-container">
             {
                 this.state.examParts.map((exam, index) =>
                     <Bar exam={exam} key={index}/>
                 )
             }
+            <ProgressBar left={percentPassed}></ProgressBar>
         </div>
+
+        <div className="timer-display">
+          {minutesLeft} : {secondsLeft} : {centiSecondsLeft}
+        </div>
+
+        
+        {this.state.timerOn === false && this.state.timerTime === 0 && (
+          <button onClick={this.startTimer}>Start</button>
+        )}
+        {this.state.timerOn === true && (
+          <button onClick={this.stopTimer}>Stop</button>
+        )}
+        {this.state.timerOn === false && this.state.timerTime > 0 && (
+          <button onClick={this.startTimer}>Resume</button>
+        )}
+        {this.state.timerOn === false && this.state.timerTime > 0 && (
+          <button onClick={this.resetTimer}>Reset</button>
+        )}
+        </>
     )
   }
 

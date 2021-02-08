2fd0a921716685db6b9d03ec821f8eeb65bec933
Luke <30535366+Luky584@users.noreply.github.com>
2021-02-08 11:26:11 +0100

    FrenGP logo added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import frengp_logo from './frengp_logo.png';
 import './App.css';
 import BarContainer from "./components/BarContainer";
 
@@ -10,6 +11,8 @@ function App() {
         <BarContainer></BarContainer>
 
         <p className="copyright">
+          <img src={frengp_logo} className="frengp-logo" alt="logo"/>FrenGP.cz
+          <br/>
           Made with React<img src={logo} className="App-logo" alt="logo" />
           <br/>
           © Lukáš Lanča 2021
